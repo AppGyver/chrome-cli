a00a2998a8e7b5c8b86a09e4cd48bdc6e91dcd17
Matti Paksula <matti.paksula@iki.fi>
2014-10-30 16:04:52 +0200

    improved deploy script, changed package naming

--- appgyver-publish.sh
+++ appgyver-publish.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env sh
 
-PACKAGE_NAME=chrome-cli-osx.tgz
-
 cd npm
 rm *.tgz
 npm pack
 
+echo "enter to deploy"
+read
 
-mv chrome-cli* $PACKAGE_NAME
-s3cmd put --acl-public --guess-mime-type $PACKAGE_NAME s3://appgyver-steroids
-rm chrome-cli-osx.tgz
+s3cmd put --acl-public --guess-mime-type *.tgz s3://appgyver-steroids
+rm *.tgz
